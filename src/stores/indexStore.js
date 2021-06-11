d5ae12d7808bff267cc32ce12cb37abaa6a5ebd1
JONATHAN EAST <JONATHAN_EAST@YMAIL.COM>
2021-06-11 07:45:52 -0400

    added redux devtools extension to check redux on server

--- src/stores/indexStore.js
+++ src/stores/indexStore.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from "redux"
 import thunk from 'redux-thunk'
 import indexReducer from "../reducers/indexReducer"
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 
 const indexStore = () => {
     return createStore(
-        indexReducer, applyMiddleware(thunk)
+        indexReducer, composeWithDevTools(applyMiddleware(thunk))
     )
 }
 
